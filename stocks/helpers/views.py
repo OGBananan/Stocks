3dccde7d6276d21e1cf7233259c84813849bafce
Anurag Bansal <anurag2003bansal@gmail.com>
2024-07-22 23:44:07 +0530

    [Change] basic import fixes

--- stocks/helpers/views.py
+++ stocks/helpers/views.py
@@ -1,49 +1,47 @@
-# yourapp/controllers.py
-
-from django.http import HttpRequest, JsonResponse
+from django.http import JsonResponse
 from django.views import View
-from error_helper import throw_error
-from schemas.test_api import (TestPostRequest,
-                            TestPostResponse,
-                            TestGetResponse,
-                            TestPatchRequest,
-                            TestPatchResponse,
-                            TestDeleteResponse)
-from schemas.request_helper import PostController
+from .schemas.test_api import (TestPostRequest,
+                                TestPostResponse,
+                                TestGetResponse,
+                                TestPatchRequest,
+                                TestPatchResponse,
+                                TestDeleteResponse)
+from .schemas.request_helper import PostController
 from .models import HealthCheck
-from django.utils.decorators import classonlymethod
 from asgiref.sync import sync_to_async
 
 
 class TestHealth(PostController, View):
 
-    @classonlymethod
+    @classmethod
     def as_view(cls, **initkwargs):
         view = super().as_view(**initkwargs)
         view._is_coroutine = lambda _: True
         return view
 
-    async def get(self, request: HttpRequest) -> JsonResponse:
+    async def get(self, request) -> JsonResponse:
         try:
             latest_health = await sync_to_async(HealthCheck.objects.latest)('created_at')
             response_data = TestGetResponse(latest_health_check={
-                                            "health": latest_health.health, "timestamp": latest_health.created_at})
+                "health": latest_health.health,
+                "timestamp": latest_health.created_at
+            })
             return self.serialize_response(TestGetResponse, response_data.dict())
         except HealthCheck.DoesNotExist:
             return JsonResponse({"error": "No health check records found"}, status=404)
         except ValueError as e:
             return JsonResponse({"error": str(e)}, status=400)
 
-    async def post(self, request: HttpRequest) -> JsonResponse:
+    async def post(self, request) -> JsonResponse:
         try:
             request_data = self.serialize_request(request, TestPostRequest)
-            new_health_check = await sync_to_async(HealthCheck.objects.acreate)(health=request_data.health)
+            new_health_check = await sync_to_async(HealthCheck.objects.create)(health=request_data.health)
             response_data = TestPostResponse(success=new_health_check.health)
             return self.serialize_response(TestPostResponse, response_data.dict())
         except ValueError as e:
             return JsonResponse({"error": str(e)}, status=400)
 
-    async def patch(self, request: HttpRequest) -> JsonResponse:
+    async def patch(self, request) -> JsonResponse:
         try:
             request_data = self.serialize_request(request, TestPatchRequest)
             latest_health = await sync_to_async(HealthCheck.objects.latest)('created_at')
@@ -56,7 +54,7 @@ class TestHealth(PostController, View):
         except ValueError as e:
             return JsonResponse({"error": str(e)}, status=400)
 
-    async def delete(self, request: HttpRequest) -> JsonResponse:
+    async def delete(self, request) -> JsonResponse:
         try:
             latest_health = await sync_to_async(HealthCheck.objects.latest)('created_at')
             await sync_to_async(latest_health.delete)()
